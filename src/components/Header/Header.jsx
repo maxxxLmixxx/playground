a4fc3eab66433a517d76545418831b3f51302db8
Maksim Shamal <max280599@gmail.com>
2020-07-22 14:46:25 +0300

    next: bubble bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { useSpring, useTransition, animated } from 'react-spring'
+import React, { useState, useEffect, useRef } from 'react'
+import { useSpring, useTransition, useChain, animated } from 'react-spring'
 
 import ReactLogo from '../../assets/images/react-logo.png'
 import Arrow from '../../assets/images/down-arrow.svg'
@@ -7,12 +7,13 @@ import Arrow from '../../assets/images/down-arrow.svg'
 import ScrollArrows from './ScrollArrows/ScrollArrows'
 
 import './styles/header-start-page.scss'
-import './styles/header-minimized.scss'
 
 const AnimatedLeftHeader = (props) => {
   const transition = useTransition(null, {
-    from: { transform: 'translateX(-100%)' },
-    enter: { transform: 'translateX(0%)' },
+    from: { opacity: 0, transform: 'translateX(-100%)' },
+    enter: { opacity: 1, transform: 'translateX(0%)' },
+    leave: { opacity: 0 },
+    ref: props.refAnimation,
   })
 
   return transition((style) => {
@@ -22,8 +23,9 @@ const AnimatedLeftHeader = (props) => {
 
 const AnimatedRightHeader = (props) => {
   const transition = useTransition(null, {
-    from: { transform: 'translateX(100%)' },
-    enter: { transform: 'translateX(0%)' },
+    from: { transform: 'translateX(10vh)' },
+    enter: { transform: 'translateX(0vh)' },
+    ref: props.refAnimation,
   })
 
   return transition((style) => {
@@ -31,7 +33,24 @@ const AnimatedRightHeader = (props) => {
   })
 }
 
+const AnimatedHeaderStart = (props) => {
+  const transition = useTransition(null, {
+    from: { transform: 'translateY(-100vh)' },
+    enter: { transform: 'translateY(0vh)' },
+    leave: { transform: 'translateY(20vh)' },
+    ref: props.refAnimation,
+  })
+  return transition((animation) => <animated.div {...props} style={animation}></animated.div>)
+}
+
 export default function Header() {
+  const refs = {
+    sizeDecrease: useRef(),
+    bubbleHeader: useRef(),
+    leftHeader: useRef(),
+    rightHeader: useRef(),
+  }
+
   const [isScrollEvent, setScrollEvent] = useState(false)
 
   const handleScroll = (e) => {
@@ -45,31 +64,28 @@ export default function Header() {
   }, [])
 
   const sizeDecrease = useSpring({
-    height: !isScrollEvent ? '100vh' : '10vh',
+    height: !isScrollEvent ? '100vh' : '7vh',
+    ref: refs.sizeDecrease,
   })
 
-  const bubble = useSpring({
-    transform: !isScrollEvent ? 'translateY(0vh)' : 'translateY(-40vh)',
-  })
+  useChain([refs.sizeDecrease, refs.bubbleHeader, refs.leftHeader, refs.rightHeader])
 
   return (
     <>
       <animated.header className="header" style={sizeDecrease}>
         {isScrollEvent && (
-          <AnimatedLeftHeader className="left-header">
-            <img className="react-logo-little" src={ReactLogo} alt="react-logo" />
-          </AnimatedLeftHeader>
+          <>
+            <AnimatedLeftHeader refAnimation={refs.leftHeader} className="left-header">
+              <img className="react-logo--small" src={ReactLogo} alt="react-logo" />
+            </AnimatedLeftHeader>
+            <AnimatedRightHeader refAnimation={refs.rightHeader} className="right-header" />
+          </>
         )}
 
-        {isScrollEvent && (
-          <AnimatedRightHeader className="right-header" />
-
-        )}
-
-        <animated.div className="header-start" style={bubble}>
+        <AnimatedHeaderStart refAnimation={refs.bubbleHeader} className="header-start">
           <img className="react-logo" src={ReactLogo} alt="react-logo" />
           <ScrollArrows arrowNumber={5} arrowElement={Arrow} />
-        </animated.div>
+        </AnimatedHeaderStart>
       </animated.header>
     </>
   )
